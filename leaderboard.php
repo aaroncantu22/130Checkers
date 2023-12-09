32f81cabb0a17709b126a55fb959f59e76baea3b
Aaron Cantu <102998464+aaroncantu22@users.noreply.github.com>
2023-12-08 21:32:50 -0800

    Add files via upload

--- leaderboard.php
+++ leaderboard.php
@@ -1,1 +1,35 @@
+<?php
+// Replace these values with your actual database connection details
+$servername = "localhost";
+$username = "your_username";
+$password = "your_password";
+$dbname = "checkersleaderboard";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Check if the form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Collect user input from the form
+    $userName = $_POST["userName"];
+    $wins = $_POST["wins"];
+
+    // Insert data into the leaderboard table
+    $sql = "INSERT INTO leaderboard (userName, wins) VALUES ('$userName', '$wins')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+// Close the database connection
+$conn->close();
+?>
 
